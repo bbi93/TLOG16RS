7263d4eeb1d356021ce4379ac4328ac70fc969e0
Bence Bors <bbors@precognox.com>
2017-10-27 15:37:27 +0200

    add resources

---
+++ src/main/java/com/bbi93/tlog16rs/resources/TLOG16RSResource.java
@@ -0,0 +1,41 @@
+package com.bbi93.tlog16rs.resources;
+
+import com.bbi93.tlog16rs.core.Greeting;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+public class TLOG16RSResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getGreeting() {
+		return "Hello world!";
+	}
+
+	@Path("/path_param/{name}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getNamedGreeting(@PathParam(value = "name") String name) {
+        return "Hello " + name;
+    }
+
+	@Path("/query_param")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getNamedStringWithParam(@DefaultValue("world") @QueryParam("name") String name) {
+        return "Hello " + name;
+    }
+
+	@Path("/hello_json")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Greeting getJSONGreeting() {
+        return new Greeting("Hello world!");
+    }
+
+}
