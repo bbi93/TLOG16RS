af6ebb3e94d681f3f55acf4ea8c441c0152f4773
Bence Bors <bbors@precognox.com>
2017-11-03 15:24:19 +0100

    add FinishingTaskRB class

---
+++ src/main/java/com/bbi93/tlog16rs/core/beans/FinishingTaskRB.java
@@ -0,0 +1,23 @@
+package com.bbi93.tlog16rs.core.beans;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author bbi93
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class FinishingTaskRB {
+
+	private int year;
+	private int month;
+	private int day;
+	private String taskId;
+	private String startTime;
+	private String endTime;
+
+}
